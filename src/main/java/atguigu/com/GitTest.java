0a832d20a8e9b93ddef1a8f59acd6dfae477014e
fucaiquan <fucaiquan@bonc.com.cn>
2022-05-24 20:18:35 +0800

    HotFix冲突第一版

--- src/main/java/atguigu/com/GitTest.java
+++ src/main/java/atguigu/com/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("第三版代码");
         System.out.println("HotFix 第一个版本");
         System.out.println("HotFix 第二个版本");
+        System.out.println("HotFix 冲突版本");
     }
 }
