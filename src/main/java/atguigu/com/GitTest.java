48c9e74e172f14e59eab3e8235f09e69a71dfc1e
fucaiquan <fucaiquan@bonc.com.cn>
2022-05-24 20:15:32 +0800

    HotFix第二个版本

--- src/main/java/atguigu/com/GitTest.java
+++ src/main/java/atguigu/com/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("第一版代码");
         System.out.println("第三版代码");
         System.out.println("HotFix 第一个版本");
+        System.out.println("HotFix 第二个版本");
     }
 }
